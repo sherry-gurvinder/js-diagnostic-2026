92f9339c82b5781a3f2146f5bbd3311b9bfaac80
sherry-gurvinder <77814528+sherry-gurvinder@users.noreply.github.com>
2026-03-03 13:03:41 -0500

    Add async function to fetch user data
    Implement async/await fetch with error handling.

---
+++ fetch.js
@@ -0,0 +1,15 @@
+// Task 4: async/await fetch with try/catch
+// Use any public API:
+// https://jsonplaceholder.typicode.com/users
+
+async function main() {
+  try {
+    // TODO: fetch data
+    // TODO: convert to json
+    // TODO: console.log something useful (like first user name)
+  } catch (err) {
+    console.error("Fetch failed:", err);
+  }
+}
+
+main();
