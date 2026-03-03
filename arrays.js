df9183e981044cf8eb779466fba313f8d73f7db2
sherry-gurvinder <77814528+sherry-gurvinder@users.noreply.github.com>
2026-03-03 13:03:01 -0500

    Add sumBy and uniqueBy function stubs

---
+++ arrays.js
@@ -0,0 +1,13 @@
+// Task 1: sumBy
+// sumBy([{a:1},{a:2}], x => x.a) -> 3
+function sumBy(arr, fn) {
+  // TODO
+}
+
+// Task 2: uniqueBy
+// uniqueBy([{id:1},{id:1},{id:2}], x => x.id) -> [{id:1},{id:2}]
+function uniqueBy(arr, fn) {
+  // TODO
+}
+
+module.exports = { sumBy, uniqueBy };
